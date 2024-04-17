c15611d843ad677a00560a80ee3a5525dcffa9eb
Abhishek Chaurasiya <abhichaurasiya2022@gmail.com>
2024-04-18 01:22:23 +0530

    Initial app setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-center justify-items-center p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl items-center  font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Getting started&nbsp;
+          <code className="font-mono font-bold">grooveify</code>
+        </p>
+
+      </div>
+
+
+
+    </main>
+  );
+}

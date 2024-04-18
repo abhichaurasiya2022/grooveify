c5ec12e2c942f933511e4a5e655af4b9d826bae2
Abhishek Chaurasiya <abhichaurasiya2022@gmail.com>
2024-04-18 12:10:59 +0530

    Implemented Theme toggle

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../ThemeToggle"
+
+
+
+
+
+export default function Homepage() {
+
+    return (
+        <>
+            <div className="max-h-4">
+                <ThemeToggle />
+
+            </div>
+        </>
+    )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                try {
+                  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                    document.documentElement.classList.add('dark')
+                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
+                  } else {
+                    document.documentElement.classList.remove('dark')
+                  }
+                } catch (_) {}
+              `,
+          }}
+        />
+      </Head>
+      <body className="flex min-h-screen bg-slate-200 dark:bg-gray-950">
         <Main />
         <NextScript />
+        <script> </script>
       </body>
     </Html>
   );

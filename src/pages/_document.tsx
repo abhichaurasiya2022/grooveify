267f1a093152b6d6d0c4ac1021a91f21908575d5
Abhishek Chaurasiya <abhichaurasiya2022@gmail.com>
2025-02-04 10:10:07 +0530

    Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,14 @@ export default function Document() {
               `,
           }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6JT7ZNM43E"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6JT7ZNM43E');`}
+        </script>
       </Head>
       <body className="w-dvw h-dvh max-h-dvh scroll-smooth bg-light-bgBody dark:bg-dark-bgBody">
         <Main />

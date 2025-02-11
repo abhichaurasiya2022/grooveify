6d846661f911754ab4877872cc47856a471ba041
Abhishek Chaurasiya <abhichaurasiya2022@gmail.com>
2025-02-11 09:48:23 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </Head>
       <body className="w-dvw h-dvh max-h-dvh scroll-smooth bg-light-bgBody dark:bg-dark-bgBody">
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LDRQVW4"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+height="0" width="0" className="hidden"></iframe></noscript>
 
         <Main />
         <NextScript />
